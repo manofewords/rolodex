ee24417804897d8b134d9c04a41693fa9b373717
Manuel Hitz <manuel.hitz@gmail.com>
2017-08-05 13:27:12 +0200

    basic API that stores data in memory
    create a card: `$ curl -H "Content-Type: application/json" -X POST -d
    '{"firstName":"Manuel","lastName":"Hitz"}' http://localhost:3000/cards`
    returns the JSON `[{“firstName":"Manuel","lastName":"Hitz”}]`
    
    getting the list of cards: `$ curl http://localhost:3000/cards`

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+var cards = [];
 
 app.use(express.static('dist'));
+app.use(bodyParser.json());
+
+app.get('/cards', function (request, response) {
+  response.json(cards);
+});
+
+app.post('/cards', function (request, response) {
+  cards.push(request.body);
+  response.json(cards);
+});
 
 app.listen(3000);
