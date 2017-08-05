ee88fd21c4fc181cc925821054a9a4c71a17a487
Manuel Hitz <manuel.hitz@gmail.com>
2017-08-05 14:20:47 +0200

    use a CSS reset
    needs dedicated webpack loaders

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 var Backbone = require('backbone');
 
+require('reset-css/reset.css');
+
 var CardsCollection = require('./collections/cards');
 var NewCard = require('./views/newCard');
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports = {
         rules: [{
             test: /\.html$/,
             use: 'html-loader'
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader', 
+                'css-loader'
+            ]
         }]
     }
 };
